234cab82eed946e1790b15cdfea53201570fa01e
uzlezz-soft <76591527+uzlezz-soft@users.noreply.github.com>
2022-03-21 18:30:46 +0300

    Create example.h

---
+++ example.h
@@ -0,0 +1,406 @@
+#include <string>
+#include <iostream>
+
+struct FFuck {};
+
+#define GENERATED_BigAss_BODY() \
+public: static const char* ClassName() { return "BigAss"; } \
+void* Invoke(const char* name, void** params = nullptr, void* out = nullptr) {\
+if(name=="V1")this->V1();\
+else if(name=="V2")this->V2();\
+else if(name=="IsFuck")*((bool *)out) = this->IsFuck();\
+else if(name=="LeshaLox")*((bool *)out) = this->LeshaLox();\
+else if(name=="GetAssId")\
+{float& p0 = *(float*)params[0];\
+*((float *)out) = this->GetAssId(p0);}\
+else if(name=="Que")\
+{int* p0 = (int*)params[0];\
+*((int *)out) = this->Que(p0);}\
+else if(name=="FUUUUCK")*((std::string *)out) = this->FUUUUCK();\
+else if(name=="Fuck1")\
+{void* p0 = (void*)params[0];\
+*((float *)out) = this->Fuck1(p0);}\
+else if(name=="Fuck2")\
+{void* p0 = (void*)params[0];\
+*((float *)out) = this->Fuck2(p0);}\
+else if(name=="Fuck3")\
+{void* p0 = (void*)params[0];\
+*((float *)out) = this->Fuck3(p0);}\
+else if(name=="Fuck4")\
+{void* p0 = (void*)params[0];\
+*((float *)out) = this->Fuck4(p0);}\
+else if(name=="Fuck5")\
+{void* p0 = (void*)params[0];\
+*((float *)out) = this->Fuck5(p0);}\
+else if(name=="Fuck6")\
+{void* p0 = (void*)params[0];\
+*((float *)out) = this->Fuck6(p0);}\
+else if(name=="Fuck7")\
+{void* p0 = (void*)params[0];\
+*((float *)out) = this->Fuck7(p0);}\
+else if(name=="Fuck8")\
+{void* p0 = (void*)params[0];\
+*((float *)out) = this->Fuck8(p0);}\
+else if(name=="Fuck9")\
+{void* p0 = (void*)params[0];\
+*((float *)out) = this->Fuck9(p0);}\
+else if(name=="Fuck10")\
+{void* p0 = (void*)params[0];\
+*((float *)out) = this->Fuck10(p0);}\
+else if(name=="Fuck11")\
+{void* p0 = (void*)params[0];\
+*((float *)out) = this->Fuck11(p0);}\
+else if(name=="Fuck12")\
+{void* p0 = (void*)params[0];\
+*((float *)out) = this->Fuck12(p0);}\
+else if(name=="Fuck13")\
+{void* p0 = (void*)params[0];\
+*((float *)out) = this->Fuck13(p0);}\
+else if(name=="Fuck14")\
+{void* p0 = (void*)params[0];\
+*((float *)out) = this->Fuck14(p0);}\
+else if(name=="Fuck15")\
+{void* p0 = (void*)params[0];\
+*((float *)out) = this->Fuck15(p0);}\
+else if(name=="Fuck16")\
+{void* p0 = (void*)params[0];\
+*((float *)out) = this->Fuck16(p0);}\
+else if(name=="Fuck17")\
+{void* p0 = (void*)params[0];\
+*((float *)out) = this->Fuck17(p0);}\
+else if(name=="Fuck18")\
+{void* p0 = (void*)params[0];\
+*((float *)out) = this->Fuck18(p0);}\
+else if(name=="Fuck19")\
+{void* p0 = (void*)params[0];\
+*((float *)out) = this->Fuck19(p0);}\
+else if(name=="Fuck20")\
+{void* p0 = (void*)params[0];\
+*((float *)out) = this->Fuck20(p0);}\
+else if(name=="Fuck21")\
+{void* p0 = (void*)params[0];\
+*((float *)out) = this->Fuck21(p0);}\
+else if(name=="Fuck22")\
+{void* p0 = (void*)params[0];\
+*((float *)out) = this->Fuck22(p0);}\
+else if(name=="Fuck23")\
+{void* p0 = (void*)params[0];\
+*((float *)out) = this->Fuck23(p0);}\
+else if(name=="Fuck24")\
+{void* p0 = (void*)params[0];\
+*((float *)out) = this->Fuck24(p0);}\
+else if(name=="Fuck25")\
+{void* p0 = (void*)params[0];\
+*((float *)out) = this->Fuck25(p0);}\
+else if(name=="Fuck26")\
+{void* p0 = (void*)params[0];\
+*((float *)out) = this->Fuck26(p0);}\
+else throw "Can't invoke method";}\
+static void* Invoke(void* t, const char* name, void** params = nullptr, void* out = nullptr) {\
+if(name=="V1")((BigAss*)t)->V1();\
+else if(name=="V2")((BigAss*)t)->V2();\
+else if(name=="IsFuck")*((bool *)out) = ((BigAss*)t)->IsFuck();\
+else if(name=="LeshaLox")*((bool *)out) = ((BigAss*)t)->LeshaLox();\
+else if(name=="GetAssId")\
+{float& p0 = *(float*)params[0];\
+*((float *)out) = ((BigAss*)((BigAss*)t))->GetAssId(p0);}\
+else if(name=="Que")\
+{int* p0 = (int*)params[0];\
+*((int *)out) = ((BigAss*)((BigAss*)t))->Que(p0);}\
+else if(name=="FUUUUCK")*((std::string *)out) = ((BigAss*)t)->FUUUUCK();\
+else if(name=="Fuck1")\
+{void* p0 = (void*)params[0];\
+*((float *)out) = ((BigAss*)((BigAss*)t))->Fuck1(p0);}\
+else if(name=="Fuck2")\
+{void* p0 = (void*)params[0];\
+*((float *)out) = ((BigAss*)((BigAss*)t))->Fuck2(p0);}\
+else if(name=="Fuck3")\
+{void* p0 = (void*)params[0];\
+*((float *)out) = ((BigAss*)((BigAss*)t))->Fuck3(p0);}\
+else if(name=="Fuck4")\
+{void* p0 = (void*)params[0];\
+*((float *)out) = ((BigAss*)((BigAss*)t))->Fuck4(p0);}\
+else if(name=="Fuck5")\
+{void* p0 = (void*)params[0];\
+*((float *)out) = ((BigAss*)((BigAss*)t))->Fuck5(p0);}\
+else if(name=="Fuck6")\
+{void* p0 = (void*)params[0];\
+*((float *)out) = ((BigAss*)((BigAss*)t))->Fuck6(p0);}\
+else if(name=="Fuck7")\
+{void* p0 = (void*)params[0];\
+*((float *)out) = ((BigAss*)((BigAss*)t))->Fuck7(p0);}\
+else if(name=="Fuck8")\
+{void* p0 = (void*)params[0];\
+*((float *)out) = ((BigAss*)((BigAss*)t))->Fuck8(p0);}\
+else if(name=="Fuck9")\
+{void* p0 = (void*)params[0];\
+*((float *)out) = ((BigAss*)((BigAss*)t))->Fuck9(p0);}\
+else if(name=="Fuck10")\
+{void* p0 = (void*)params[0];\
+*((float *)out) = ((BigAss*)((BigAss*)t))->Fuck10(p0);}\
+else if(name=="Fuck11")\
+{void* p0 = (void*)params[0];\
+*((float *)out) = ((BigAss*)((BigAss*)t))->Fuck11(p0);}\
+else if(name=="Fuck12")\
+{void* p0 = (void*)params[0];\
+*((float *)out) = ((BigAss*)((BigAss*)t))->Fuck12(p0);}\
+else if(name=="Fuck13")\
+{void* p0 = (void*)params[0];\
+*((float *)out) = ((BigAss*)((BigAss*)t))->Fuck13(p0);}\
+else if(name=="Fuck14")\
+{void* p0 = (void*)params[0];\
+*((float *)out) = ((BigAss*)((BigAss*)t))->Fuck14(p0);}\
+else if(name=="Fuck15")\
+{void* p0 = (void*)params[0];\
+*((float *)out) = ((BigAss*)((BigAss*)t))->Fuck15(p0);}\
+else if(name=="Fuck16")\
+{void* p0 = (void*)params[0];\
+*((float *)out) = ((BigAss*)((BigAss*)t))->Fuck16(p0);}\
+else if(name=="Fuck17")\
+{void* p0 = (void*)params[0];\
+*((float *)out) = ((BigAss*)((BigAss*)t))->Fuck17(p0);}\
+else if(name=="Fuck18")\
+{void* p0 = (void*)params[0];\
+*((float *)out) = ((BigAss*)((BigAss*)t))->Fuck18(p0);}\
+else if(name=="Fuck19")\
+{void* p0 = (void*)params[0];\
+*((float *)out) = ((BigAss*)((BigAss*)t))->Fuck19(p0);}\
+else if(name=="Fuck20")\
+{void* p0 = (void*)params[0];\
+*((float *)out) = ((BigAss*)((BigAss*)t))->Fuck20(p0);}\
+else if(name=="Fuck21")\
+{void* p0 = (void*)params[0];\
+*((float *)out) = ((BigAss*)((BigAss*)t))->Fuck21(p0);}\
+else if(name=="Fuck22")\
+{void* p0 = (void*)params[0];\
+*((float *)out) = ((BigAss*)((BigAss*)t))->Fuck22(p0);}\
+else if(name=="Fuck23")\
+{void* p0 = (void*)params[0];\
+*((float *)out) = ((BigAss*)((BigAss*)t))->Fuck23(p0);}\
+else if(name=="Fuck24")\
+{void* p0 = (void*)params[0];\
+*((float *)out) = ((BigAss*)((BigAss*)t))->Fuck24(p0);}\
+else if(name=="Fuck25")\
+{void* p0 = (void*)params[0];\
+*((float *)out) = ((BigAss*)((BigAss*)t))->Fuck25(p0);}\
+else if(name=="Fuck26")\
+{void* p0 = (void*)params[0];\
+*((float *)out) = ((BigAss*)((BigAss*)t))->Fuck26(p0);}\
+else throw "Can't invoke method";}\
+void* GetMember(const char* name) {\
+if(name=="m_fuck1")return (void*)&(this->m_fuck1);\
+else if(name=="m_fuck2")return (void*)&(this->m_fuck2);\
+else if(name=="m_fuck3")return (void*)&(this->m_fuck3);\
+else if(name=="m_fuck4")return (void*)&(this->m_fuck4);\
+else if(name=="m_fuck5")return (void*)&(this->m_fuck5);\
+else if(name=="m_fuck6")return (void*)&(this->m_fuck6);\
+else if(name=="m_fuck7")return (void*)&(this->m_fuck7);\
+else if(name=="m_fuck8")return (void*)&(this->m_fuck8);\
+else if(name=="m_fuck10")return (void*)&(this->m_fuck10);\
+else if(name=="m_fuck11")return (void*)(this->m_fuck11);\
+else if(name=="m_fuck12")return (void*)(this->m_fuck12);\
+else if(name=="m_fuck13")return (void*)(this->m_fuck13);\
+else if(name=="m_fuck14")return (void*)&(this->m_fuck14);\
+else if(name=="m_fuck15")return (void*)&(this->m_fuck15);\
+else if(name=="m_fuck16")return (void*)&(this->m_fuck16);\
+else if(name=="m_fuck17")return (void*)&(this->m_fuck17);\
+else if(name=="m_fuck18")return (void*)&(this->m_fuck18);\
+else if(name=="m_fuck19")return (void*)&(this->m_fuck19);\
+else if(name=="m_fuck20")return (void*)&(this->m_fuck20);\
+else if(name=="m_fuck21")return (void*)(this->m_fuck21);\
+else if(name=="m_fuck22")return (void*)(this->m_fuck22);\
+else if(name=="m_fuck23")return (void*)(this->m_fuck23);\
+else if(name=="m_fuck24")return (void*)&(this->m_fuck24);\
+else if(name=="m_fuck25")return (void*)&(this->m_fuck25);\
+else if(name=="m_fuck26")return (void*)&(this->m_fuck26);\
+else if(name=="m_fuck27")return (void*)&(this->m_fuck27);\
+else if(name=="m_fuck28")return (void*)&(this->m_fuck28);\
+else if(name=="m_fuck29")return (void*)&(this->m_fuck29);\
+else if(name=="m_fuck30")return (void*)&(this->m_fuck30);\
+else throw "Can't find member"; return nullptr;}\
+static void* GetMember(void* t, const char* name) {\
+if(name=="m_fuck1")return (void*)&(((BigAss*)t)->m_fuck1);\
+else if(name=="m_fuck2")return (void*)&(((BigAss*)t)->m_fuck2);\
+else if(name=="m_fuck3")return (void*)&(((BigAss*)t)->m_fuck3);\
+else if(name=="m_fuck4")return (void*)&(((BigAss*)t)->m_fuck4);\
+else if(name=="m_fuck5")return (void*)&(((BigAss*)t)->m_fuck5);\
+else if(name=="m_fuck6")return (void*)&(((BigAss*)t)->m_fuck6);\
+else if(name=="m_fuck7")return (void*)&(((BigAss*)t)->m_fuck7);\
+else if(name=="m_fuck8")return (void*)&(((BigAss*)t)->m_fuck8);\
+else if(name=="m_fuck10")return (void*)&(((BigAss*)t)->m_fuck10);\
+else if(name=="m_fuck11")return (void*)(((BigAss*)t)->m_fuck11);\
+else if(name=="m_fuck12")return (void*)(((BigAss*)t)->m_fuck12);\
+else if(name=="m_fuck13")return (void*)(((BigAss*)t)->m_fuck13);\
+else if(name=="m_fuck14")return (void*)&(((BigAss*)t)->m_fuck14);\
+else if(name=="m_fuck15")return (void*)&(((BigAss*)t)->m_fuck15);\
+else if(name=="m_fuck16")return (void*)&(((BigAss*)t)->m_fuck16);\
+else if(name=="m_fuck17")return (void*)&(((BigAss*)t)->m_fuck17);\
+else if(name=="m_fuck18")return (void*)&(((BigAss*)t)->m_fuck18);\
+else if(name=="m_fuck19")return (void*)&(((BigAss*)t)->m_fuck19);\
+else if(name=="m_fuck20")return (void*)&(((BigAss*)t)->m_fuck20);\
+else if(name=="m_fuck21")return (void*)(((BigAss*)t)->m_fuck21);\
+else if(name=="m_fuck22")return (void*)(((BigAss*)t)->m_fuck22);\
+else if(name=="m_fuck23")return (void*)(((BigAss*)t)->m_fuck23);\
+else if(name=="m_fuck24")return (void*)&(((BigAss*)t)->m_fuck24);\
+else if(name=="m_fuck25")return (void*)&(((BigAss*)t)->m_fuck25);\
+else if(name=="m_fuck26")return (void*)&(((BigAss*)t)->m_fuck26);\
+else if(name=="m_fuck27")return (void*)&(((BigAss*)t)->m_fuck27);\
+else if(name=="m_fuck28")return (void*)&(((BigAss*)t)->m_fuck28);\
+else if(name=="m_fuck29")return (void*)&(((BigAss*)t)->m_fuck29);\
+else if(name=="m_fuck30")return (void*)&(((BigAss*)t)->m_fuck30);\
+else throw "Can't find member"; return nullptr;}\
+void SetMember(const char* name, void* value) {\
+if(name=="m_fuck1")this->m_fuck1=*((int*)value);\
+else if(name=="m_fuck2")this->m_fuck2=*((int*)value);\
+else if(name=="m_fuck3")this->m_fuck3=*((float*)value);\
+else if(name=="m_fuck4")this->m_fuck4=*((float*)value);\
+else if(name=="m_fuck5")this->m_fuck5=*((float*)value);\
+else if(name=="m_fuck6")this->m_fuck6=*((float*)value);\
+else if(name=="m_fuck7")this->m_fuck7=*((int*)value);\
+else if(name=="m_fuck8")this->m_fuck8=*((int*)value);\
+else if(name=="m_fuck10")this->m_fuck10=*((int*)value);\
+else if(name=="m_fuck11")this->m_fuck11=(char*)value;\
+else if(name=="m_fuck12")this->m_fuck12=(char*)value;\
+else if(name=="m_fuck13")this->m_fuck13=(char*)value;\
+else if(name=="m_fuck14")this->m_fuck14=*((int*)value);\
+else if(name=="m_fuck15")this->m_fuck15=*((int*)value);\
+else if(name=="m_fuck16")this->m_fuck16=*((FFuck*)value);\
+else if(name=="m_fuck17")this->m_fuck17=*((FFuck*)value);\
+else if(name=="m_fuck18")this->m_fuck18=*((FFuck*)value);\
+else if(name=="m_fuck19")this->m_fuck19=*((FFuck*)value);\
+else if(name=="m_fuck20")this->m_fuck20=*((int*)value);\
+else if(name=="m_fuck21")this->m_fuck21=(int*)value;\
+else if(name=="m_fuck22")this->m_fuck22=(int*)value;\
+else if(name=="m_fuck23")this->m_fuck23=(int*)value;\
+else if(name=="m_fuck24")this->m_fuck24=*((int*)value);\
+else if(name=="m_fuck25")this->m_fuck25=*((long long*)value);\
+else if(name=="m_fuck26")this->m_fuck26=*((int*)value);\
+else if(name=="m_fuck27")this->m_fuck27=*((int*)value);\
+else if(name=="m_fuck28")this->m_fuck28=*((int*)value);\
+else if(name=="m_fuck29")this->m_fuck29=*((int*)value);\
+else if(name=="m_fuck30")this->m_fuck30=*((int*)value);\
+else throw "Can't find member";}\
+static void SetMember(void* t, const char* name, void* value) {\
+if(name=="m_fuck1")((BigAss*)t)->m_fuck1=*((int*)value);\
+else if(name=="m_fuck2")((BigAss*)t)->m_fuck2=*((int*)value);\
+else if(name=="m_fuck3")((BigAss*)t)->m_fuck3=*((float*)value);\
+else if(name=="m_fuck4")((BigAss*)t)->m_fuck4=*((float*)value);\
+else if(name=="m_fuck5")((BigAss*)t)->m_fuck5=*((float*)value);\
+else if(name=="m_fuck6")((BigAss*)t)->m_fuck6=*((float*)value);\
+else if(name=="m_fuck7")((BigAss*)t)->m_fuck7=*((int*)value);\
+else if(name=="m_fuck8")((BigAss*)t)->m_fuck8=*((int*)value);\
+else if(name=="m_fuck10")((BigAss*)t)->m_fuck10=*((int*)value);\
+else if(name=="m_fuck11")((BigAss*)t)->m_fuck11=(char*)value;\
+else if(name=="m_fuck12")((BigAss*)t)->m_fuck12=(char*)value;\
+else if(name=="m_fuck13")((BigAss*)t)->m_fuck13=(char*)value;\
+else if(name=="m_fuck14")((BigAss*)t)->m_fuck14=*((int*)value);\
+else if(name=="m_fuck15")((BigAss*)t)->m_fuck15=*((int*)value);\
+else if(name=="m_fuck16")((BigAss*)t)->m_fuck16=*((FFuck*)value);\
+else if(name=="m_fuck17")((BigAss*)t)->m_fuck17=*((FFuck*)value);\
+else if(name=="m_fuck18")((BigAss*)t)->m_fuck18=*((FFuck*)value);\
+else if(name=="m_fuck19")((BigAss*)t)->m_fuck19=*((FFuck*)value);\
+else if(name=="m_fuck20")((BigAss*)t)->m_fuck20=*((int*)value);\
+else if(name=="m_fuck21")((BigAss*)t)->m_fuck21=(int*)value;\
+else if(name=="m_fuck22")((BigAss*)t)->m_fuck22=(int*)value;\
+else if(name=="m_fuck23")((BigAss*)t)->m_fuck23=(int*)value;\
+else if(name=="m_fuck24")((BigAss*)t)->m_fuck24=*((int*)value);\
+else if(name=="m_fuck25")((BigAss*)t)->m_fuck25=*((long long*)value);\
+else if(name=="m_fuck26")((BigAss*)t)->m_fuck26=*((int*)value);\
+else if(name=="m_fuck27")((BigAss*)t)->m_fuck27=*((int*)value);\
+else if(name=="m_fuck28")((BigAss*)t)->m_fuck28=*((int*)value);\
+else if(name=="m_fuck29")((BigAss*)t)->m_fuck29=*((int*)value);\
+else if(name=="m_fuck30")((BigAss*)t)->m_fuck30=*((int*)value);\
+else throw "Can't find member";}
+
+class BigAss
+{
+public:
+	
+	GENERATED_BigAss_BODY()
+	
+	BigAss() {}
+	~BigAss() {}
+	
+	void V1() { std::cout << "V1\n"; }
+	void V2() { std::cout << "V2\n"; }
+	
+	bool IsFuck() { return false; }
+	bool LeshaLox() { return true; }
+	
+	float GetAssId(float& fuck) { return m_fuck3; }
+	int Que(int* i) { return *i; }
+	
+	std::string FUUUUCK() { return "FUUUUCK"; }
+	
+	float Fuck1(void* fuckery) { return 0; }
+	float Fuck2(void* fuckery) { return 0; }
+	float Fuck3(void* fuckery) { return 0; }
+	float Fuck4(void* fuckery) { return 5; }
+	float Fuck5(void* fuckery) { return 0; }
+	float Fuck6(void* fuckery) { return 3; }
+	float Fuck7(void* fuckery) { return 2; }
+	float Fuck8(void* fuckery) { return 1; }
+	float Fuck9(void* fuckery) { return 0; }
+	float Fuck10(void* fuckery) { return 1; }
+	float Fuck11(void* fuckery) { return 1; }
+	float Fuck12(void* fuckery) { return 1; }
+	float Fuck13(void* fuckery) { return 1; }
+	float Fuck14(void* fuckery) { return 1; }
+	float Fuck15(void* fuckery) { return 1; }
+	float Fuck16(void* fuckery) { return 1; }
+	float Fuck17(void* fuckery) { return 1; }
+	float Fuck18(void* fuckery) { return 1; }
+	float Fuck19(void* fuckery) { return 1; }
+	float Fuck20(void* fuckery) { return 1; }
+	float Fuck21(void* fuckery) { return 1; }
+	float Fuck22(void* fuckery) { return 1; }
+	float Fuck23(void* fuckery) { return 1; }
+	float Fuck24(void* fuckery) { return 1; }
+	float Fuck25(void* fuckery) { return 1; }
+	float Fuck26(void* fuckery) { return 1; }
+	
+private:
+	
+	int m_fuck1;
+	int m_fuck2;
+	float m_fuck3;
+	float m_fuck4;
+	float m_fuck5;
+	float m_fuck6;
+	int m_fuck7;
+	int m_fuck8;
+	int m_fuck10;
+	char* m_fuck11;
+	char* m_fuck12;
+	char* m_fuck13;
+	int m_fuck14;
+	int m_fuck15;
+	FFuck m_fuck16;
+	FFuck m_fuck17;
+	FFuck m_fuck18;
+	FFuck m_fuck19;
+	int m_fuck20;
+	int* m_fuck21;
+	int* m_fuck22;
+	int* m_fuck23;
+	int m_fuck24;
+	long long m_fuck25;
+	int m_fuck26;
+	int m_fuck27;
+	int m_fuck28;
+	int m_fuck29;
+	int m_fuck30;
+	
+};
+
+int main()
+{
+    void* object = new BigAss();
+    
+    float result;
+    BigAss::Invoke(object, "Fuck6", nullptr, &result);
+    
+    std::cout << result;
+    
+    return 0;
+}
